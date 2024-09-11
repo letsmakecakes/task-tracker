b967d075f6afbf2abcfcbd94efdf2f9bf876ad70
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-09-12 00:10:01 +0530

    added enum and related methods

--- internal/task/status.go
+++ internal/task/status.go
@@ -1,1 +1,37 @@
 package task
+
+import "errors"
+
+type TaskStatus int
+
+const (
+	Todo TaskStatus = iota
+	InProgress
+	Done
+)
+
+var statusNames = []string{
+	"todo",
+	"in-progress",
+	"done",
+}
+
+// String returns the string representation of a TaskStatus
+func (s TaskStatus) String() string {
+	if s < Todo || s > Done {
+		return "unknown"
+	}
+
+	return statusNames[s]
+}
+
+// ParseStatus converts a string to a TaskStatus enum
+func ParseStatus(status string) (TaskStatus, error) {
+	for i, name := range statusNames {
+		if name == status {
+			return TaskStatus(i), nil
+		}
+	}
+
+	return TaskStatus(-1), errors.New("invalid task status")
+}
