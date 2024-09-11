ff83a28a04aade2d308918bdcbe31c403d34aa89
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-09-12 00:02:55 +0530

    added struct to hold task objects

--- internal/task/task.go
+++ internal/task/task.go
@@ -1,1 +1,11 @@
 package task
+
+import "time"
+
+type Task struct {
+	ID          string    `json:"id" mapstructure:"id"`
+	Description string    `json:"description" mapstructure:"description"`
+	Status      string    `json:"status" mapstructure:"status"`
+	CreatedAt   time.Time `json:"created_at" mapstructure:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at" mapstructure:"updated_at"`
+}
