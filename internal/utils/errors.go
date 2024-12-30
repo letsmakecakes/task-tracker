cbaebe1bb89ae14f7ac919c9e8874bd53b61efd6
Adwaith Rajeev <adwaithrajeev@gmail.com>
2024-12-30 15:30:18 +0530

    Removed unwanted custom error messages.

--- internal/utils/errors.go
+++ internal/utils/errors.go
@@ -2,11 +2,9 @@ package utils
 
 import "errors"
 
-// Define custom error messages for the application.
 var (
 	ErrInvalidCommand = errors.New("invalid command")
 	ErrInvalidArgs    = errors.New("invalid arguments")
 	ErrInvalidID      = errors.New("invalid task ID")
-	ErrTaskNotFound   = errors.New("task not found")
 	ErrInvalidStatus  = errors.New("invalid status")
 )
